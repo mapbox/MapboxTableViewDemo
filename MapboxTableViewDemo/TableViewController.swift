9ef92f67084c3a721d7b0f99e9d6bad77104e509
Justin R. Miller <incanus@codesorcery.net>
2015-09-01 21:54:58 -0700

    reuse maps

--- MapboxTableViewDemo/TableViewController.swift
+++ MapboxTableViewDemo/TableViewController.swift
@@ -4,11 +4,15 @@ import Mapbox
 class TableViewController: UITableViewController {
 
     let identifier = "MapboxCell"
+    var maxMapCount: UInt!
+    var maps = [MGLMapView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.registerClass(NSClassFromString("UITableViewCell"), forCellReuseIdentifier: identifier)
+
+        maxMapCount = UInt(view.bounds.size.height / tableView(tableView, heightForRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0))) + 2
     }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -32,13 +36,25 @@ class TableViewController: UITableViewController {
             cell.layer.borderColor = UIColor.redColor().colorWithAlphaComponent(0.5).CGColor
             cell.layer.borderWidth = 10
 
-            let map = MGLMapView(frame: cell.bounds)
-            map.logoView.hidden = true
-            map.attributionButton.hidden = true
-            map.userInteractionEnabled = false
-            cell.addSubview(map)
-        } else {
-            cell = UITableViewCell(style: .Default, reuseIdentifier: identifier)
+            var map: MGLMapView?
+
+            for existingMap in maps {
+                let mapRect = existingMap.convertRect(existingMap.frame, toView: view)
+                if CGRectIntersection(mapRect, view.frame) == CGRectNull {
+                    map = existingMap
+                    println("reused existing map for row \(indexPath.row)")
+                }
+            }
+
+            if map == nil {
+                let map = MGLMapView(frame: cell.bounds)
+                map.logoView.hidden = true
+                map.attributionButton.hidden = true
+                map.userInteractionEnabled = false
+                cell.addSubview(map)
+                maps.append(map)
+                println("made new map \(maps.count - 1) for row \(indexPath.row)")
+            }
         }
 
         return cell
